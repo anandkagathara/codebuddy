5fd1e7736c812b9858a22ebedd2a98c438c25207
Anand Kagatahra <112871902+anandkagathara7@users.noreply.github.com>
2023-10-06 17:42:29 +0530

    initial commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,10 +2,62 @@ const User = require("../schema/user.schema");
 
 module.exports.getUsersWithPostCount = async (req, res) => {
   try {
-    //TODO: Implement this API
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
 
-    res.status(200).json({});
+    const skip = (page - 1) * limit;
+
+    const usersAggregate = [
+      {
+        $match: {},
+      },
+      {
+        $lookup: {
+          from: 'posts',
+          localField: '_id',
+          foreignField: 'userId', 
+          as: 'posts',
+        },
+      },
+      {
+        $project: {
+          name: 1,
+          posts: { $size: '$posts' }, 
+        },
+      },
+      {
+        $skip: skip,
+      },
+      {
+        $limit: limit,
+      },
+    ];
+
+    const users = await User.aggregate(usersAggregate);
+    const totalDocs = await User.countDocuments();
+    const totalPages = Math.ceil(totalDocs / limit);
+    const hasPrevPage = page > 1;
+    const hasNextPage = page < totalPages;
+
+    const response = {
+      data: {
+        users,
+        pagination: {
+          totalDocs,
+          limit,
+          page,
+          totalPages,
+          pagingCounter: skip + 1,
+          hasPrevPage,
+          hasNextPage,
+          prevPage: hasPrevPage ? page - 1 : null,
+          nextPage: hasNextPage ? page + 1 : null,
+        },
+      },
+    };
+    res.status(200).json(response);
   } catch (error) {
     res.send({ error: error.message });
   }
 };
+
